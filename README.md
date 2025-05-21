619b0edb6bdea941640e823b09c0694c729d4d27
Macbook Air <macbookair@Macbooks-MacBook-Air.local>
2025-05-21 18:56:51 +0300

    Remove unwanted files from previous commits

--- README.md
+++
@@ -1,2 +0,0 @@
-# Kevin-Naluyange
-Std_No: 1800723394 RegNo: 18/U/23394/EVE
--- practice.py
+++
@@ -1,7 +0,0 @@
-print('Hello Buddy!')
-print('What is your name?')
-myName = input()
-print('Nice to meet you ' +myName)
-print() #puts a blank line on the screem
-print('The length of your name is: ' +str(len(myName)))
-#print(len(myName))
